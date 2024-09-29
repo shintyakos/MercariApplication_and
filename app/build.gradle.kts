10e1e3f7c70e0cbb01d4fc0d344dc78cd9e824ca
shintyakos <shintyakos1145@gmail.com>
2024-09-29 23:47:16 +0900

    MER-1 タイトル画面作成 Update themes and add new Kotlin setup
    Changed the color scheme in Theme.kt and updated the Gradle wrapper version. Added new drawables, Kotlin lint configuration, and setup for Kotlin 1.9 including new plugins. Also, made structural updates to MainActivity and several project settings files.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jlleitschuh.gradle.ktlint") version "10.1.0"
 }
 
 android {
@@ -27,11 +28,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -63,4 +64,5 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation("androidx.compose.ui:ui-text-google-fonts:1.7.0")
 }
--- app/src/main/java/com/stake/mercariapplication/MainActivity.kt
+++ app/src/main/java/com/stake/mercariapplication/MainActivity.kt
@@ -3,13 +3,24 @@ package com.stake.mercariapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.stake.mercariapplication.ui.theme.Blue60
 import com.stake.mercariapplication.ui.theme.MercariApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +28,41 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MercariApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Surface {
+                    Title()
                 }
             }
         }
     }
 }
 
+@Suppress("ktlint:standard:function-naming")
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Title() {
+    Column(modifier = Modifier.fillMaxSize().padding(start = 14.dp, end = 14.dp, top = 24.dp, bottom = 24.dp)) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.End,
+        ) {
+            Text(
+                text = "スキップ",
+                fontFamily = FontFamily(Font(R.font.notosansjp_medium)),
+                fontSize = 14.sp,
+                color = Blue60,
+            )
+        }
+        Image(
+            painter = painterResource(R.drawable.mercari_service_primary_horizontal),
+            contentDescription = "title",
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@Suppress("ktlint:standard:function-naming")
+@Preview(device = "id:pixel_6a")
 @Composable
 fun GreetingPreview() {
     MercariApplicationTheme {
-        Greeting("Android")
+        Title()
     }
 }
